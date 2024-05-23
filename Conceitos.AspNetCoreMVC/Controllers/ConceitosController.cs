222bbad86ae922bfe90a7e234d921db6fb951dcb
Maísa <maisasantos2003@gmail.com>
2024-05-23 15:14:38 -0300

    Adicionar arquivos de projeto.

---
+++ Conceitos.AspNetCoreMVC/Controllers/ConceitosController.cs
@@ -0,0 +1,50 @@
+﻿using Conceitos.AspNetCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Conceitos.AspNetCoreMVC.Controllers
+{
+    public class ConceitosController : Controller
+    {
+        // Dentro de um controlle os métodos são chamados de Action
+        public IActionResult Index() //interface
+        {
+            return View();
+        }
+
+        // Exemplos de Actions
+        // 1. Action retornando uma string
+        public string MostrarText()
+        {
+            return "Texto obtido no Controller Conceitos";
+        }
+
+        // 2. Action retornando um arquivo pdf
+        public FileResult MostrarPdf()
+        {
+            // Parametros -> nome do arquivo, tipo de conteudo
+            return File("~/pdf/Desenvolvimento Web com AspNet MVC e AspNet Core.pdf", "application/pdf");
+        }
+
+        // 3. Action retornando uma View
+        public IActionResult MostrarConteudo()
+        {
+            return View();
+        }
+
+        // 4. Action enviando os dados de um produto para a View
+        public IActionResult MostrarProduto()
+        {
+            Produto produto = new Produto()
+            {
+                Codigo = 10, Descricao = "Caixa de Som", Preco = 200
+            };
+            return View(produto);
+        }
+
+        // 5. Action enviado uma lista de produtos para a View
+        public IActionResult MostrarLista()
+        {
+            return View(Informacoes.ListarProdutos());
+        }
+    }
+}
---
+++ Conceitos.AspNetCoreMVC/Models/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace Conceitos.AspNetCoreMVC.Models
+{
+    public class Produto
+    {
+        // prop (atalho de propriedade)
+        public int Codigo { get; set; }
+        public string? Descricao { get; set; }
+        // ? -> mostra que sabe que pode ter o valor null em strings
+        public double Preco { get; set; }
+    }
+}
